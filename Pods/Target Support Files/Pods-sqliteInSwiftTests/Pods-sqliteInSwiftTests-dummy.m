9efefd51c6bea5cc2710fedc47b7932b0e5e0859
Jenny Chang <jennychang@Jennys-MacBook-Pro-2.local>
2019-02-12 14:01:04 -0800

    removing pod files from github

--- Pods/Target Support Files/Pods-sqliteInSwiftTests/Pods-sqliteInSwiftTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sqliteInSwiftTests : NSObject
-@end
-@implementation PodsDummy_Pods_sqliteInSwiftTests
-@end
--- Pods/Target Support Files/Pods-sqliteInSwiftUITests/Pods-sqliteInSwiftUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sqliteInSwiftUITests : NSObject
-@end
-@implementation PodsDummy_Pods_sqliteInSwiftUITests
-@end
